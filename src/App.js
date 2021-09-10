d30d7b0db42c0af9061d3cdf9137bf2139e55cb2
gourav-24 <gauravdeswal06@gmail.com>
2021-09-10 17:59:09 +0530

    my react for heroku deployment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div> <p> Hey my first react app </p> </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
